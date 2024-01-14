69d1d78b33b39bc8035e942cb3f936dbcf47184e
Shakirali2 <iamshakir3@gmail.com>
2024-01-15 00:27:39 +0500

    nextjs-contentful

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      CONTENTFUL_SPACE_ID: "6eopkuvu0lol",
+      CONTENTFUL_ENVIRONMENT: "master",
+      },
+}
+
+module.exports = nextConfig
